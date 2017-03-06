c36e16d1384507ff759f7b838c6cb910179964e4
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-03-06 14:17:47 +0100

    Fallbacks if the request json is not valid

--- src/intents/new-assets.js
+++ src/intents/new-assets.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const { AssetsQueryOptions } = require('drequest-maxdome');
 
 module.exports = async (request, response, { maxdome }) => {
@@ -7,8 +8,8 @@ module.exports = async (request, response, { maxdome }) => {
       .addFilter('contentTypeSeriesOrMovies')
       .addQuery('pageSize', 1)
       .addSort('activeLicenseStart', 'desc');
-  if (request.params.genre) {
-    assetsQueryOptions.addFilter('genre', request.params.genre);
+  if (_.has(request, 'params.genre')) {
+    assetsQueryOptions.addFilter('genre', _.get(request, 'params.genre'));
   }
   const asset = (await maxdome.request('assets').send(assetsQueryOptions))[0];
   response.render('asset', { asset });
--- src/intents/search-assets.js
+++ src/intents/search-assets.js
@@ -1,11 +1,12 @@
+const _ = require('lodash');
 const { AssetsQueryOptions } = require('drequest-maxdome');
 
 module.exports = async (request, response, { maxdome }) => {
   const assetsQueryOptions =
     new AssetsQueryOptions()
       .addFilter('contentTypeSeriesOrMovies')
-      .addFilter('search', request.params.search)
       .addQuery('pageSize', 1);
+      .addFilter('search', _.get(request, 'params.search'))
   const asset = (await maxdome.request('assets').send(assetsQueryOptions))[0];
   response.render('asset', { asset });
 };
