1b9246f2053f3b65f3be60a41b25dd2cae54d935
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-24 11:50:48 +0200

    Remove tip of the day

--- src/intents/tipOfTheDay.js
+++
@@ -1,4 +0,0 @@
-module.exports = async (request, response, { maxdome }) => {
-  const tipOfTheDay = (await maxdome.request('tipOfTheDays').send())[0];
-  response.render('tipOfTheDay', { tipOfTheDay });
-};
