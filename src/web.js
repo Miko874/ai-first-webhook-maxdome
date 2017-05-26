24ddae59dc6b4538d6a9bfbf813c495ceab824b1
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-26 15:21:00 +0200

    Upgrade "@dnode/request-maxdome" and env for all options

--- src/web.js
+++ src/web.js
@@ -11,7 +11,14 @@ i18n.configure({
   updateFiles: false,
   locales: ['en', 'de'],
 });
-const maxdome = require('@dnode/request-maxdome').getRequestBuilder();
+const maxdome = require('@dnode/request-maxdome').getRequestBuilder({
+  maxdomeOptions: {
+    apikey: process.env.MAXDOME_APIKEY,
+    appid: process.env.MAXDOME_APPID,
+    hostname: process.env.MAXDOME_HOSTNAME,
+    protocol: process.env.MAXDOME_PROTOCOL,
+  }
+});
 
 require('@dnode/controllers')(app, [
   require('./controller')({ i18n, maxdome }),
